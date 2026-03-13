2cfa036e54d60f88444382119a34fb0018ef5cd9
ritam-g <ritammaty2006@gmail.com>
2026-03-13 23:31:10 +0530

    feat: add static file serving and catch-all route for frontend
    Added Express static file serving for the public directory and a catch-all route to serve the frontend index.html file, enabling proper SPA routing for the YouTube downloader application.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const downloadRouter = require("./routes/download.route");
+const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static("./public"))
 
 
 app.use('/api', downloadRouter)
 
+app.get('*any',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','\\public\\index.html'))
+})
 module.exports = app
