3151500c81a9ac7f060dff7c6d0b3ffcf6368e6c
ritam-g <ritammaty2006@gmail.com>
2026-03-20 13:15:14 +0530

    feat: initialize Express app with CORS, JSON parsing, static files, and download routes.

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,14 +6,19 @@ const path = require('path')
 const app = express()
 
 app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Private-Network', 'true');
+    next();
+});
 app.use(cors({
-    origin:"https://youtube-dowenloder.vercel.app"
+    origin: ["https://youtube-dowenloder.vercel.app", "http://localhost:5173","https://tubeflow-backend.onrender.com"],
+    credentials: true
 }))
 app.use(express.static("./public"))
 
 app.use('/api', downloadRouter)
 
-app.get('*', (req, res) => {
+app.get('*any', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 module.exports = app
